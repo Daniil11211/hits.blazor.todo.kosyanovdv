c3206c887616fcfb91ad49cbc16d115536fd8d6c
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2025-11-21 13:25:46 +0500

    Исходный код трека №2

---
+++ ToDoServerApp/Data/Interfaces/IDataService.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoServerApp.Data.Interfaces
+{
+    public interface IDataService
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task SaveAsync(TaskItem item);
+        Task<TaskItem> GetTaskAsync(int id);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ ToDoServerApp/Data/Migrations/20251120134356_AddRest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 20, 18, 43, 53, 352, DateTimeKind.Local).AddTicks(7078));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 20, 18, 43, 53, 352, DateTimeKind.Local).AddTicks(7106));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 20, 18, 43, 53, 352, DateTimeKind.Local).AddTicks(7110));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 20, 18, 43, 53, 352, DateTimeKind.Local).AddTicks(7113));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 15, 0, 38, 29, 525, DateTimeKind.Local).AddTicks(7687));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 15, 0, 38, 29, 525, DateTimeKind.Local).AddTicks(7715));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 15, 0, 38, 29, 525, DateTimeKind.Local).AddTicks(7719));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 15, 0, 38, 29, 525, DateTimeKind.Local).AddTicks(7722));
+        }
+    }
+}
